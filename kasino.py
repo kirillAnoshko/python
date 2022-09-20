d6514b5fd2675d6441a1e1fd137b9daa562db390
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2022-09-20 19:24:20 +0300

    Add files via upload

---
+++ kasino.py
@@ -0,0 +1,31 @@
+import random
+
+user_money = 2
+casino_money = 2
+
+
+while user_money and casino_money:
+    print("У игрока", user_money, "монет")
+    print("У казино", casino_money, "монет")
+
+    input("нажмите Enter чтобы сделать ход")
+
+    user_turn = random.randint(1, 6)
+    casino_turn = random.randint(1, 6)
+    print("игрок выбросил", user_turn)
+    print("казино выбросило", casino_turn)
+    if user_turn > casino_turn:
+        print("игрок победил")
+        user_money += 1
+        casino_money -= 1
+    elif casino_turn > user_turn:
+        print("казино победило")
+        casino_money += 1
+        user_money -= 1
+    else:
+        print("ничья")
+
+if user_money:
+    print("Игрок забрал все деньги")
+else:
+    print("Казино забрало все деньги")  
