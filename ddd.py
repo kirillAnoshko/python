1ae2b3e4753877bd282bcf285f00cb422c3fb954
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2022-10-07 19:46:34 +0300

    Add files via upload

---
+++ ddd.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("blue")
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.left(90)
+turtle.fd(100)
+turtle.right(45)
+turtle.fd(40)
+turtle.right(100)
+turtle.fd(40)
+
+turtle.done()
