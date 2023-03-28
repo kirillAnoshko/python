23881f34f6926f8d7221a5e0e0c6ae9668d1f833
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2023-03-28 19:44:02 +0300

    Add files via upload

---
+++ hero.py
@@ -0,0 +1,17 @@
+import os
+
+class Hero:
+
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.inventory = []
+        
+
+
+hero = Hero("Вася", 100, 10)
+enemy = Hero("Враг", 100, 5)s            
+
+
+
